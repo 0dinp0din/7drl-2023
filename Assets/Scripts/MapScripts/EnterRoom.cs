903d8f5cc45fe798a597d65c977f82b84c851bdf
odin <odin481@gmail.com>
2023-03-07 14:21:37 +0100

    Doors open when room is cleared

--- Assets/Scripts/MapScripts/EnterRoom.cs
+++ Assets/Scripts/MapScripts/EnterRoom.cs
@@ -8,7 +8,8 @@ public class EnterRoom : MonoBehaviour
 {
     public GameObject[] enemies;
     public GameObject[] enemySpawns;
-
+    public GameObject door;
+    
     private bool hasTriggered = false;
     
     private void OnTriggerEnter(Collider other)
@@ -17,11 +18,20 @@ public class EnterRoom : MonoBehaviour
         {
             foreach (var enemy in enemySpawns)
             {
-                int rand = Random.Range(0, enemies.Length);
+                int rand = Random.Range(0, enemies.Length); 
                 Instantiate(enemies[rand], enemy.transform.position, Quaternion.identity);
             }
 
             hasTriggered = true;
+        }
+    }
+    
+    private void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (hasTriggered && enemies.Length == 0 )
+        {
+            Destroy(door);
         }
     }
 
